d92167807ece962358a5b46cc75bbe30e64816c4
Ilias <ilias.benjelloun@depinfonancy.net>
2021-12-25 18:25:20 +0100

    Fix position of background with top and left css property

--- src/Background.jsx
+++ src/Background.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container, AppBar } from '@material-ui/core';
+import { Button, Container, AppBar, Hidden } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { mergeClasses } from '@material-ui/styles';
 
@@ -7,16 +7,13 @@ const useStyles = makeStyles({
     root: ({bimg}) => {
         return {
             backgroundImage: `url(${bimg})`,
+            WebkitBackgroundSize: 'cover',
             backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            height: '100%',
-            width: '100%',
-            margin: 'auto',
-            position: 'absolute',
+            backgroundPosition: 'center center',
+            minHeight: '100%',
+            minWidth: '100%',
+            position: "fixed",
             top: 0,
-            right: 0,
-            bottom: 0,
             left: 0
         }
     }
